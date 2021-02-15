071057165e7e33497631dc7458ad653fe75fc4eb
karl <karl.whelan26@mail.dcu.ie>
2021-02-15 17:29:48 +0000

    Replace button n\vigation on frontend with navbar
    - Add navbar with link to each page
    
    - Highlight active link
    
    - Add clickable burger menu which displays dropdown navbar
    
    - Hide and display navbar and burger menu based on screen size

--- src/client/src/components/Header.js
+++ src/client/src/components/Header.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react"
 import "./Header.css"
+import Navbar from "./Navbar"
 
 class Header extends Component {
   render() {
+
     return (
-      <header data-testid="Header" className="navbar">
+      <header data-testid="Header">
        <title>Crowdfunding DApp</title>
-       <div className ="HeadingContainer">
-         <h1>Blockchain Crowdfunding</h1>
-       </div>
+       <nav className="navbar">
+        <div className= "appTitle">Blockchain Crowdfunding</div>
+        <div onClick={() => {
+          this.props.handleBurgerMenuClick(!this.props.burgerMenuClicked)
+        }} className="burgerMenu">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <div className={"navbarLinks " + (this.props.burgerMenuClicked
+                                          ? 'menuClicked' : '')}>
+            <Navbar handlePageChange={this.props.handlePageChange}
+                    currentPage={this.props.currentPage}/>
+        </div>
+       </nav>
       </header>
     )
   }
